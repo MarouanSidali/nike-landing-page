79c02943ba3e875a8c84505356dd33f8feecf748
MarouanSidali <marwandx57@gmail.com>
2023-08-29 14:17:27 +0000

    Created The project strucutre

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Subscribe,
+    Footer,
+    CustomerReviews,
+    Services,
+    SpecialOffer
+}
