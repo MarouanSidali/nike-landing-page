b98b2aeda0f639387d3c6fbb32caeaef11962a18
MarouanSidali <marwandx57@gmail.com>
2023-09-04 08:47:22 +0000

    Fixed the padding bug

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     <main className="relative">
       <Nav />
       <section
-        className="xl:padding-1 wide:padding-r
+        className="xl:padding-l wide:padding-r
       padding-b"
       >
         <Hero />
