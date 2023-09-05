d727931bd79809896287eeb736501a4accee37ce
MarouanSidali <marwandx57@gmail.com>
2023-09-05 13:19:00 +0000

    Completed the super quality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,8 @@ const Button = ({label,iconURL}) => {
     leading-none bg-coral-red rounded-full text-white
      border-coral-red">
         {label}
-        <img src={iconURL} alt="Arrow Right"
-        className="rounded-full ml-2 w-5 h-5 " />
+        {iconURL && <img src={iconURL} alt="Arrow Right"
+        className="rounded-full ml-2 w-5 h-5 " />}
     
     
     </button>
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col
+    gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2
+          className="font-palanquin text-4xl capitalize
+         font-bold lg:max-w-lg"
+        >
+          We Provide You
+          <span className="text-coral-red "> Super</span>
+          <span className="text-coral-red ">Quality</span> Shoes
+        </h2>
+        <p className="mt-4 info-text lg:max-w-lg">
+          Discover stylish arrival, quality comfort, and innovation for your
+          active life.
+        </p>
+        <p className="mt-6 info-text lg:max-w-lg">
+          our dedication to details and exellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img src={shoe8} alt="shoe8"
+        width={570}
+        height={522}
+        className="object-contain" />
 
-export default SuperQuality
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
