e189f1e7e7474cd38da98f33601868d456604c8c
MarouanSidali <marwandx57@gmail.com>
2023-09-05 12:45:54 +0000

    Fixed the gap isue

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -13,7 +13,7 @@ const PopularProducts = () => {
         </p>
       </div>
       <div
-        className="mt-16 grid lg-grid-cols-4 md:grid-cols-3
+        className="mt-16 grid lg-grid-cols-4 md:grid-cols-4
       sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14"
       >
         {products.map((products) => (
